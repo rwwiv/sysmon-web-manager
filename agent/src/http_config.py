f156ba14c2b10a09b69013d530c5d8a6b3f7e4e6
William Wernert <william.wernert@gmail.com>
2019-02-17 11:24:31 -0500

    Initial work on background process for agent app.
    Currently the process is runnning in a single thread and everything is blocking. The plan is to move to threads in a future commit.

---
+++ agent/src/http_config.py
@@ -0,0 +1,11 @@
+import configparser
+
+__config = configparser.ConfigParser().read('../config.ini')['http']
+
+auth_user = __config['AuthUser']
+auth_pass = __config['AuthPass']
+agent_uuid = None if (__config['AgentUUID'] == 'None') else __config['AgentUUID']
+base_url = __config['BaseURL']
+api_heartbeat = __config['ApiHeartbeat']
+api_get_sysmon = __config['ApiGetSysmon']
+api_get_config = __config['ApiGetConfig']

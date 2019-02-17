198a9b6e28b5bdc7ccfbb06bc096db35f2a8a15a
William Wernert <william.wernert@gmail.com>
2019-02-17 11:14:14 -0500

    Initial work on agent application.

---
+++ agent/src/sysmon.py
@@ -0,0 +1,24 @@
+import subprocess
+
+import sysmon_config
+
+
+def install_sysmon():
+    # arguments = f''
+    # for arg in config.installer_args:
+    #     arguments += f'-{arg}'
+
+    print(subprocess
+          .call(f'{sysmon_config.cwd}\\{sysmon_config.sysmon_exec_name}'
+                f' -accepteula -i {sysmon_config.cwd}\\..\\{sysmon_config.config_file_name}', shell=True))
+
+
+def update_sysmon_config():
+    print(subprocess
+          .call(f'{sysmon_config.cwd}\\{sysmon_config.sysmon_exec_name}'
+                f' -c {sysmon_config.cwd}\\..\\{sysmon_config.config_file_name}', shell=True))
+
+
+def uninstall_sysmon():
+    print(subprocess
+          .call(f'{sysmon_config.cwd}\\..\\{sysmon_config.sysmon_exec_name} -u', shell=True))

7abb51b209daf03e35c77a54ba09a0a2bf3d7254
Aubrey Bryant <aubbryant@augusta.edu>
2019-02-18 21:15:02 -0500

    Update Configs.vue
    fixing some minor errors

--- server/frontend/src/views/Configs.vue
+++ server/frontend/src/views/Configs.vue
@@ -14,100 +14,101 @@
              @edit="editConfig"
              @view="viewConfig"
              />
-            </ul> 
+            </ul>
             <p v-else>
               No configurations. Click add new to make a new configuration.
             </p>
             -->
     </div>
-	  	<div class="config row">
-	  		<div class="col-xs-6">
-	  			<h3>Available Configurations</h3>
-	  			<!-- Named Configurations -->
-	  			<!-- To do: need a component to drive li element generation -->
-		    	<ul class="list-group list-all-configs">
-		    		<li class="list-group-item" id="configID_default">
-		    			<div class="box box-solid box-host">
-		    				<div class="box-header">
-		    					<h4 class="box-title"><i class="fa fa-circle text-success"></i>Default Configuration</h4>
-		    					<div class="box-tools pull-right">
+      <div class="config row">
+        <div class="col-xs-6">
+          <h3>Available Configurations</h3>
+          <!-- Named Configurations -->
+          <!-- To do: need a component to drive li element generation -->
+          <ul class="list-group list-all-configs">
+            <li class="list-group-item" id="configID_default2">
+              <div class="box box-solid box-host">
+                <div class="box-header">
+                  <h4 class="box-title"><i class="fa fa-circle text-success"></i>Default Configuration</h4>
+                  <div class="box-tools pull-right">
 									<button>
-		               View
-		              </button>
+                    View
+                  </button>
 									<button>
-		               Edit
-		              </button>
+                    Edit
+                  </button>
 									<button>
-		               Delete
-		              </button>
-		    						<i class="fa fa-play"></i>
-		    						<i class="fa fa-wrench"></i>
-		    						<i class="fa fa-trash"></i>
-		    					</div>
-		    				</div>	
-		    			</div>
-		    		</li>
-		    			<li class="list-group-item" id="configID_1">
-		    			<div class="box box-solid box-host">
-		    				<div class="box-header">
-		    					<h4 class="box-title"><i class="fa fa-circle text-success"></i>Configuration 1</h4>
-		    					<div class="box-tools pull-right">
+                    Delete
+                  </button>
+                    <i class="fa fa-play"></i>
+                    <i class="fa fa-wrench"></i>
+                    <i class="fa fa-trash"></i>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item" id="configID_1">
+              <div class="box box-solid box-host">
+                <div class="box-header">
+                  <h4 class="box-title"><i class="fa fa-circle text-success"></i>Configuration 1</h4>
+                  <div class="box-tools pull-right">
 									<button>
-		               View
-		              </button>
+                    View
+                  </button>
 									<button>
-		               Edit
-		              </button>
+                    Edit
+                  </button>
 									<button>
-		               Delete
-		              </button>
-		    						<i class="fa fa-play"></i>
-		    						<i class="fa fa-wrench"></i>
-		    						<i class="fa fa-trash"></i>
-		    					</div>
-		    				</div>	
-		    			</div>
-		    		</li>
-           	<li class="list-group-item" id="configID_default">
-		    			<div class="box box-solid box-host">
-		    				<div class="box-header">
-		    					<h4 class="box-title"><i class="fa fa-circle text-success"></i>Configuration 2</h4>
-		    					<div class="box-tools pull-right">
+                    Delete
+                  </button>
+                    <i class="fa fa-play"></i>
+                    <i class="fa fa-wrench"></i>
+                    <i class="fa fa-trash"></i>
+                  </div>
+                </div>
+              </div>
+            </li>
+            <li class="list-group-item" id="configID_default">
+              <div class="box box-solid box-host">
+                <div class="box-header">
+                  <h4 class="box-title"><i class="fa fa-circle text-success"></i>Configuration 2</h4>
+                  <div class="box-tools pull-right">
 									<button>
-		               View
-		              </button>
+                    View
+                  </button>
 									<button>
-		               Edit
-		              </button>
+                    Edit
+                  </button>
 									<button>
-		               Delete
-		              </button>
-		    						<i class="fa fa-play"></i>
-		    						<i class="fa fa-wrench"></i>
-		    						<i class="fa fa-trash"></i>
-		    					</div>
-		    				</div>	
-		    			</div>
-		    		</li>
-		    	</ul>
+                    Delete
+                  </button>
+                    <i class="fa fa-play"></i>
+                    <i class="fa fa-wrench"></i>
+                    <i class="fa fa-trash"></i>
+                  </div>
+                </div>
+              </div>
+            </li>
+          </ul>
 			</div>
 			<!-- Sysmon Configs -->
 			<!-- To do:  Need a component to drive config li element generation -->
 			</div>
 	</section>
 </template>
-
-<script>
+<!--<script>
 
 import HelloWorld from './components/HelloWorld.vue';
+
+<script>
 export default {
-	components: {
-		HelloWorld
-	},
+  components: {
+    HelloWorld,
+  },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 	input[type="checkbox"]{
 		margin-right:10px;
@@ -119,7 +120,6 @@ export default {
 		max-height:400px;
 	}
 
-	
 
 	.list-group-item {
 		padding:0px;

589d169e95d15c52ab694cf2ed4666d54c8d6e23
Aubrey Bryant <aubbryant@augusta.edu>
2019-02-21 19:44:50 -0500

    Configurations display in a list; pages work with Orlando's UI

--- server/frontend/src/components/ConfigsList.vue
+++ server/frontend/src/components/ConfigsList.vue
@@ -1,4 +1,4 @@
-<div>
+<template>
   <div class="box">
     <div class="box-header with-border">
       <h4 class="box-title">
@@ -12,79 +12,53 @@
           <tr>
             <th>Name</th>
             <th>Is Default?</th>
-            <th class="center-text icon-column">Edit</th>
+            <th class = "center-text icon-column">View</th>
+            <th class = "center-text icon-column">Edit</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="config in configs" :key="config.name">
             <td>{{ config.name }}</td>
-            if(config.isDefault){
-            { <td> Default </td>
+            <td>{{ config.isDefault }}</td>
             <td class="center-text">
-              <a>
-                <i class="fa fa-play"></i>
+              <a @click="viewConfig(config.name)">
+                <i class="fa fa-search-plus"></i>
               </a>
             </td>
             <td class="center-text">
               <a @click="editConfig(config.name)">
-                <i class="fa fa-trash"></i>
+                <i class="fa fa-wrench"></i>
               </a>
             </td>
           </tr>
         </tbody>
       </table>
     </div>
-    <script>
-      import configs from '../../configsList.json';
+  </div>
+</template>
+<script>
+import configs from '../../configsList.json';
 
-      export default {
-        name: 'ConfigList',
-        data() {
-          var newConfig;
-          return {
-            configs,
-            newConfig,
-            editConfig:'',
-          }
-        },
-        methods:{
-          newConfig(config){
-            this.newConfig = config;
-          },
-          editConfig(config){
-            //To do:  need to make a call to update associated config file.
-            if(config){
-              console.log(config);
-              $('#agent-modal').modal('hide');
-            }
-            else {
-              console.log('No selection made.');
-            }
-          },
-        },
-        mounted:function(){
-          //To do:  need to access data locally on load.
-          // this.$http.get('http://localhost:80/agentSample.json').then((data)=>{
-          //   console.log(data.body)
-          // });
-        }
-      };
-    </script>
+export default {
+  name: 'ConfigsList',
+  data() {
+    return {
+      configs,
+      editConfig: '',
+      viewConfig: '',
+    };
+  },
+};
+</script>
 
-    <style scoped>
-      td a {
-        cursor:pointer;
-      }
-      .center-text {
-        text-align:center;
-      }
-      .icon-column{
-        width:10%;
-      }
-      .edit-config-link{
-        margin-left:10px;
-        cursor:pointer;
-      }
-    </style>
-</div>
-</div>
+<style scoped>
+  td a {
+    cursor:pointer;
+  }
+  .center-text {
+    text-align:center;
+  }
+  .icon-column{
+    width:10%;
+  }
+</style>

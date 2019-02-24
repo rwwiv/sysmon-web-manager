7d681aeff3dffcd63a7367282cf29f9489e3c619
William Wernert <william.wernert@gmail.com>
2019-02-24 13:00:09 -0500

    Further bugfixes
    Began fixing PEP violations in Django server, and also replaced example xml with swift config.

--- server/backend/heartbeat/heartbeatService.py
+++ server/backend/heartbeat/heartbeatService.py
@@ -1,63 +1,68 @@
 from heartbeat.models import agent
 import json
 
-def getSysmonVersion():
-    return "ExampleSysmonVersion"
 
-def getConfigName():
-    return "ExampleConfigName"
+def get_sysmon_version():
+    return "sysmon"
 
-def getSysmonUpdateFlag():
+
+def get_config_name():
+    return "example"
+
+
+def get_sysmon_update_flag():
     return False
 
-def getConfigUpdateFlag():
+
+def get_config_update_flag():
     return False
 
-def updateAgentStatus(requestedUUID, incomingRequest):
-    retrievedAgent = agent.objects.get(UUID=requestedUUID)
+
+def update_agent_status(requested_uuid, incoming_request):
+    retrieved_agent = agent.objects.get(UUID=requested_uuid)
     
-    updatesData = {}
-    updatesData ['sysmon'] = getSysmonUpdateFlag()
-    updatesData ['sysmon_version'] = retrievedAgent.SYSMON_VERSION_CURRENT
-    updatesData ['config'] = getConfigUpdateFlag()
-    updatesData ['config_name'] = retrievedAgent.CONFIG_NAME_CURRENT
+    updates_data = {
+        'sysmon': get_sysmon_update_flag(),
+        'sysmon_version': retrieved_agent.SYSMON_VERSION_CURRENT,
+        'config': get_config_update_flag(),
+        'config_name': retrieved_agent.CONFIG_NAME_CURRENT
+    }
 
-    data = {}
-    data ['updates_needed'] = updatesData
-    data ['uninstall'] = retrievedAgent.NEEDS_UNINSTALL
-    data ['restart'] = retrievedAgent.NEEDS_RESTART
+    data = {
+        'updates_needed': updates_data,
+        'uninstall': retrieved_agent.NEEDS_UNINSTALL,
+        'restart': retrieved_agent.NEEDS_RESTART
+    }
 
-    persistData = json.loads(incomingRequest.body)
-    retrievedAgent.SYSMON_VERSION_CURRENT = persistData['sysmon_version']
-    retrievedAgent.CONFIG_NAME_CURRENT = persistData['config_name']
-    retrievedAgent.EXEC_RUNNING = persistData['exec_running']
-    retrievedAgent.EXEC_LAST_RUNNING_AT = persistData['exec_last_running_at']
+    persist_data = json.loads(incoming_request.body)
+    retrieved_agent.SYSMON_VERSION_CURRENT = persist_data['sysmon_version']
+    retrieved_agent.CONFIG_NAME_CURRENT = persist_data['config_name']
+    retrieved_agent.EXEC_RUNNING = persist_data['exec_running']
+    retrieved_agent.EXEC_LAST_RUNNING_AT = persist_data['exec_last_running_at']
 
-    retrievedAgent.save()
+    retrieved_agent.save()
     return data
 
-def createAgent(requestedUUID):
 
-    if(agent.objects.filter(UUID=requestedUUID).count() == 0):
-        newAgent = agent(UUID = requestedUUID,
-        IPV4_ADDRESS = "",
-        IPV6_ADDRESS = "",
-        ONLINE = True,
-        SYSMON_VERSION_CURRENT = "",
-        SYSMON_VERSION_NEW = "",
-        CONFIG_NAME_CURRENT = "",
-        CONFIG_NAME_NEW = "",
-        EXEC_RUNNING = True,
-	NEEDS_UNINSTALL = False,
-	NEEDS_RESTART = False)
-        newAgent.save()
+def create_agent(requested_uuid):
+    if agent.objects.filter(UUID=requested_uuid).count() == 0:
+        new_agent = agent(UUID=requested_uuid,
+                          IPV4_ADDRESS="",
+                          IPV6_ADDRESS="",
+                          ONLINE=True,
+                          SYSMON_VERSION_CURRENT="",
+                          SYSMON_VERSION_NEW="",
+                          CONFIG_NAME_CURRENT="",
+                          CONFIG_NAME_NEW="",
+                          EXEC_RUNNING=True,
+                          NEEDS_UNINSTALL=False,
+                          NEEDS_RESTART=False)
+        new_agent.save()
     else:
         print("Agent already existed")
 
-    
-    
-    data = {}
-    data ['sysmon_version'] = getSysmonVersion()
-    data ['config_name'] = getConfigName()
-
+    data = {
+        'sysmon_version': get_sysmon_version(),
+        'config_name': get_config_name()
+    }
     return data

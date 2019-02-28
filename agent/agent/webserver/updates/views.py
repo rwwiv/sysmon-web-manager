b8ea7234673fb07cb34bd3cda4180bff23710104
William Wernert <william.wernert@gmail.com>
2019-02-28 16:36:56 -0500

    Removing webserver from agent

--- agent/agent/webserver/updates/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import HttpResponse
-
-def config(request):
-    return HttpResponse("Updates/config endpoint")
-
-def sysmon(request):
-    return HttpResponse("Updates/sysmon endpoint")

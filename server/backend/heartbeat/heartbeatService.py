71ad53c2111d8f6bcb5ade2fb49acb9fe714abbc
Chandler Butler <chandlerbutler0@gmail.com>
2019-03-02 10:43:39 -0500

    FIX url pattern UPDATE retrieval of sysmon and configuration version to query db CHANGE order of update agent status to update the status and then check for needing updates FIX file name format for retrieving sysmon and configuration

--- server/backend/heartbeat/heartbeatService.py
+++ server/backend/heartbeat/heartbeatService.py
@@ -1,14 +1,15 @@
 from heartbeat.models import Agent
+from heartbeat.models import Sysmon
+from heartbeat.models import Configuration
 from os import listdir
 import json
 import ipaddress
 
 def get_sysmon_version():
-    # return listdir("../sysmon")[0].split('.')[0]
-    return "sysmon_13.37.exe"
+    return Sysmon.objects.get(IS_CURRENT = True)
 
 def get_config_name():
-    return "agent_config_gifnoc.xml"
+    return Configuration.objects.get(IS_DEFAULT = True)
 
 def get_sysmon_update_flag(server_version, client_version):
     return not client_version == server_version
@@ -20,7 +21,17 @@ def get_config_update_flag(server_version, client_version):
 
 def update_agent_status(requested_uuid, incoming_request):
     retrieved_agent = Agent.objects.get(UUID=requested_uuid)
+    persist_data = json.loads(incoming_request.body)
     
+    if 'sysmon_version' in persist_data:
+        retrieved_agent.SYSMON_VERSION_CURRENT = persist_data['sysmon_version']
+    if 'config_name' in persist_data:
+        retrieved_agent.CONFIG_NAME_CURRENT = persist_data['config_name']
+    if 'exec_running' in persist_data:
+        retrieved_agent.EXEC_RUNNING = persist_data['exec_running']
+    if 'exec_last_running_at' in persist_data:
+        retrieved_agent.EXEC_LAST_RUNNING_AT = persist_data['exec_last_running_at']
+
     updates_data = {
         'sysmon': get_sysmon_update_flag(Agent.SYSMON_VERSION_NEW, Agent.SYSMON_VERSION_CURRENT),
         'sysmon_version': retrieved_agent.SYSMON_VERSION_CURRENT,
@@ -34,17 +45,6 @@ def update_agent_status(requested_uuid, incoming_request):
         'restart': retrieved_agent.NEEDS_RESTART,
         'install': retrieved_agent.NEEDS_INSTALL
     }
-
-    persist_data = json.loads(incoming_request.body)
-    if 'sysmon_version' in persist_data:
-        retrieved_agent.SYSMON_VERSION_CURRENT = persist_data['sysmon_version']
-    if 'config_name' in persist_data:
-        retrieved_agent.CONFIG_NAME_CURRENT = persist_data['config_name']
-    if 'exec_running' in persist_data:
-        retrieved_agent.EXEC_RUNNING = persist_data['exec_running']
-    if 'exec_last_running_at' in persist_data:
-        retrieved_agent.EXEC_LAST_RUNNING_AT = persist_data['exec_last_running_at']
-
     retrieved_agent.save()
     return data
 
@@ -52,14 +52,13 @@ def update_agent_status(requested_uuid, incoming_request):
 def create_agent(requested_uuid,remoteAddr):
     if Agent.objects.filter(UUID=requested_uuid).count() == 0:
         new_agent = Agent(UUID=requested_uuid,
-                          # TODO record the IP address(es) of the origin of the request
                           IP_ADDRESS=remoteAddr,
                           ONLINE=True,
                           SYSMON_VERSION_CURRENT="",
                           SYSMON_VERSION_NEW="",
                           CONFIG_NAME_CURRENT="",
                           CONFIG_NAME_NEW="",
-                          EXEC_RUNNING=False,  # sysmon should not be running when the agent calls in
+                          EXEC_RUNNING=False,
                           NEEDS_INSTALL=False,
                           NEEDS_UNINSTALL=False,
                           NEEDS_RESTART=False)

7ff4fcd5132db90450858185e18d1eff124a576c
William Wernert <william.wernert@gmail.com>
2019-03-02 19:42:08 -0500

    Changes to agent

--- server/backend/heartbeat/heartbeatService.py
+++ server/backend/heartbeat/heartbeatService.py
@@ -3,12 +3,15 @@ from os import listdir
 import json
 import ipaddress
 
+
 def get_sysmon_version():
     # return listdir("../sysmon")[0].split('.')[0]
-    return "sysmon_13.37.exe"
+    return "8.04"
+
 
 def get_config_name():
-    return "agent_config_gifnoc.xml"
+    return "gifnoc"
+
 
 def get_sysmon_update_flag(server_version, client_version):
     return not client_version == server_version
@@ -20,14 +23,23 @@ def get_config_update_flag(server_version, client_version):
 
 def update_agent_status(requested_uuid, incoming_request):
     retrieved_agent = Agent.objects.get(UUID=requested_uuid)
-    
+    if retrieved_agent.NEEDS_INSTALL:
+        updates_data = {
+            'sysmon': get_sysmon_update_flag(Agent.SYSMON_VERSION_NEW, Agent.SYSMON_VERSION_CURRENT),
+            'sysmon_version': retrieved_agent.SYSMON_VERSION_NEW,
+            'config': get_config_update_flag(Agent.CONFIG_NAME_NEW, Agent.CONFIG_NAME_CURRENT),
+            'config_name': retrieved_agent.CONFIG_NAME_NEW
+        }
+        data = {
+            'updates_needed': updates_data,
+            'uninstall': retrieved_agent.NEEDS_UNINSTALL,
+            'restart': retrieved_agent.NEEDS_RESTART,
+            'install': retrieved_agent.NEEDS_INSTALL
+        }
     updates_data = {
-        'sysmon': get_sysmon_update_flag(Agent.SYSMON_VERSION_NEW, Agent.SYSMON_VERSION_CURRENT),
-        'sysmon_version': retrieved_agent.SYSMON_VERSION_CURRENT,
-        'config': get_config_update_flag(Agent.CONFIG_NAME_NEW, Agent.CONFIG_NAME_CURRENT),
-        'config_name': retrieved_agent.CONFIG_NAME_CURRENT
+        'sysmon': False,
+        'config': False
     }
-
     data = {
         'updates_needed': updates_data,
         'uninstall': retrieved_agent.NEEDS_UNINSTALL,
@@ -49,16 +61,16 @@ def update_agent_status(requested_uuid, incoming_request):
     return data
 
 
-def create_agent(requested_uuid,remoteAddr):
+def create_agent(requested_uuid, remoteAddr):
     if Agent.objects.filter(UUID=requested_uuid).count() == 0:
         new_agent = Agent(UUID=requested_uuid,
                           # TODO record the IP address(es) of the origin of the request
                           IP_ADDRESS=remoteAddr,
                           ONLINE=True,
                           SYSMON_VERSION_CURRENT="",
-                          SYSMON_VERSION_NEW="",
+                          SYSMON_VERSION_NEW=get_sysmon_version(),
                           CONFIG_NAME_CURRENT="",
-                          CONFIG_NAME_NEW="",
+                          CONFIG_NAME_NEW=get_config_name(),
                           EXEC_RUNNING=False,  # sysmon should not be running when the agent calls in
                           NEEDS_INSTALL=False,
                           NEEDS_UNINSTALL=False,

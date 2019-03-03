b8bcbdbe5be5b8a8ff4322331cf4a43e10bd1cb3
William Wernert <william.wernert@gmail.com>
2019-03-03 11:21:48 -0500

    Initial setup for writing unit tests

--- server/frontend/tests/unit/.eslintrc.js
+++ server/frontend/tests/unit/.eslintrc.js
@@ -2,4 +2,14 @@ module.exports = {
   env: {
     mocha: true,
   },
+  rules: {
+    'no-unused-import': 'off',
+    'no-tabs': 'off',
+    'max-len': 'off',
+    'no-plusplus': 'off',
+    indent: 'off',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
 };

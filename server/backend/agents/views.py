21b279ff3c60c66d42a6e5652018500f7845fbf1
Chandler Butler <chandlerbutler0@gmail.com>
2019-03-03 20:25:28 -0500

    bug fixes and added configs endpoint so no more 404 atleast

--- server/backend/agents/views.py
+++ server/backend/agents/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import HttpResponse
 from django.http import Http404, JsonResponse, HttpResponseBadRequest
 from .agentService import get_all_agents
 from .agentService import  update_needs_install
+from .agentService import update_needs_restart
 
 def index(request):
     if request.method == 'GET':
@@ -15,9 +16,16 @@ def index(request):
 
 
 def updates(request,uuid):
-    if request.method == 'POST':
+    if request.method == 'PATCH':
         success =  update_needs_install(uuid)
         if(success == 0):
             return HttpResponse("succesful update")
         else:
             return HttpResponseBadRequest("update failed")
+    elif request.method == 'POST':
+        success = update_needs_restart(uuid)
+        if(success == 0):
+            return HttpResponse("succesful update")
+        else:
+            return HttpResponseBadRequest("update failed")
+

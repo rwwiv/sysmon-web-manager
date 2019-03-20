ae092e599bcfcf1cc2f32b8d85794223c39ee345
Aubrey Bryant <aubbryant@augusta.edu>
2019-03-20 18:54:13 -0400

    Update ConfigEditorInput.vue

--- server/frontend/src/components/ConfigEditorInput.vue
+++ server/frontend/src/components/ConfigEditorInput.vue
@@ -1,9 +1,10 @@
 <template>
   <div style="display: block;" id="configEd">
     <xml id="config" src="../sysmonconfig-export.xml"></xml>
-  <label for="configEd" id="configEdLabel">Edit configuration and then click save.</label>
+  <label for="configEd" id="configEdLabel">Edit configuration. Validate and save using the buttons below.</label>
     <div class="textWrapper"><textarea cols="2" rows="20" id="editingConfig"></textarea></div>
   <div class="col-auto">
+    <button class="btn btn-secondary pull-left">Validate</button>
     <button class="btn btn-secondary pull-right">Save Changes</button>
   </div>
   </div>

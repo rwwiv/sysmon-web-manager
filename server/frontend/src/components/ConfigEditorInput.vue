9182e4561b17c3093de6121976f26bc8b1ab0e89
Aubrey Bryant <aubbryant@augusta.edu>
2019-03-31 11:04:05 -0400

    added config rename and confirmation message

--- server/frontend/src/components/ConfigEditorInput.vue
+++ server/frontend/src/components/ConfigEditorInput.vue
@@ -3,27 +3,46 @@
     <button class= "btn btn-secondary pull-left">Select a file to add a new configuration. <input type= "file" @change="loadConfig"></button>
       <textarea id="inputTextToSave" v-model="text" @loadstart="loadConfig"></textarea>
     <button class="btn btn-secondary pull-left">Validate</button>
+    <label for="configName"> Configuration Name</label><input type="text" @change="nameConfig(configName.data)" id="configName" width="auto" v-model="text">
     <button class="btn btn-secondary pull-right" @click="saveConfig">Save Changes </button>
   </div>
 </template>
 <script>
   import axios from 'axios';
 
+  let configName;
   export default {
         // name: 'ConfigEditorInput.vue',
+
         methods: {
+          nameConfig(name) {
+            configName = name;
+          },
           loadConfig(ev) {
             const file = ev.target.files[0];
             const reader = new FileReader();
             reader.onload = function (fileLoadedEvent) {
               document.getElementById('inputTextToSave').value = fileLoadedEvent.target.result;
+              document.getElementById('configName').value = file.name;
             };
             reader.readAsText(file);
           },
           saveConfig() {
-            const textToSave = document.getElementById('inputTextToSave').value;
-            document.getElementById('inputTextToSave').value = 'File saved.';
-            // not sure how to send the text back to the server
+            let confirmMsg;
+            let responseStatus;
+            axios.get(`http://localhost:8000/configs/${configName}`).then((response) => {
+              responseStatus = response.status;
+            })
+            if (responseStatus !== '400') {
+              confirmMsg = 'You are about to save this new configuration. Press OK to proceed or cancel to make changes.';
+            } else {
+              confirmMsg = 'You are about to make changes to an existing configuration. Press OK to proceed. To make a new configuration, change the configuration name.'
+            }
+            if (confirm(confirmMsg)) {
+              const textToSave = document.getElementById('inputTextToSave').value;
+              document.getElementById('inputTextToSave').value = 'File saved.';
+            }
+            // not sure how to send the text data back to the server
           },
           viewConfig() {
             axios.get('http://localhost:8080/file.txt').then(response => response.data);

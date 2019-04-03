655a3f0e949bf25bfe46eef99f502bf942d9c176
Aubrey Bryant <aubbryant@augusta.edu>
2019-04-03 01:16:57 -0400

    fixing things after testing against the backend

--- server/frontend/src/components/ConfigEditorInput.vue
+++ server/frontend/src/components/ConfigEditorInput.vue
@@ -1,42 +1,49 @@
 <template>
   <div>
     <button class= "btn btn-secondary pull-left">Select a file to add a new configuration. <input type= "file" @change="loadConfig"></button>
-      <textarea id="inputTextToSave" v-model="text" @loadstart="loadConfig"></textarea>
+      <textarea id="inputTextToSave" v-model="text" @loadstart="loadConfig" @change="didNotValidate()"></textarea>
     <button class="btn btn-secondary pull-left" @click="validateConfig">Validate</button>
-    <label for="configName"> Configuration Name</label><input type="text" @change="nameConfig(configName.data)" id="configName" width="auto" v-model="text">
+    <label for="configNameBox"> Configuration Name</label><input type="text" @change="nameConfig(configNameBox.value)" id="configNameBox" width="auto" v-model="text">
     <button class="btn btn-secondary pull-right" @click="saveConfig">Save Changes </button>
   </div>
 </template>
 <script>
   import axios from 'axios';
 
-  let configName;
-  let didValidate = 0;
-  let XMLconfig;
-  let inputTextToSave;
   export default {
-    // name: 'ConfigEditorInput.vue',
-
+    name: 'ConfigEditorInput.vue',
+    data() {
+      return {
+        configName: '',
+        didValidate: '',
+        XMLconfig: XMLDocument,
+        inputTextToSave: '',
+      }
+    },
     methods: {
       nameConfig(name) {
-        configName = name;
+        this.configName = name;
       },
       loadConfig(ev) {
         const file = ev.target.files[0];
         const reader = new FileReader();
         reader.onload = function (fileLoadedEvent) {
           document.getElementById('inputTextToSave').value = fileLoadedEvent.target.result;
-          document.getElementById('configName').value = file.name;
+          document.getElementById('configNameBox').value = file.name;
         };
         reader.readAsText(file);
+      },
+      didNotValidate(){
+        this.didValidate = 0;
       },
       saveConfig() {
         let confirmMsg;
         let responseStatus;
-        if (didValidate === 0) {
+        if (this.didValidate === 0) {
           confirm('Please validate before saving.');// this works
         } else {
-          axios.get(`http://localhost:8000/configs/${configName}`).then((response) => {
+          this.configName = document.getElementById('configNameBox').value;
+          axios.get('http://localhost:8000/configs/', this.configName).then((response) => {
             responseStatus = response.status;
           });
           if (responseStatus !== '400') {
@@ -45,8 +52,12 @@
             confirmMsg = 'You are about to make changes to an existing configuration. Press OK to proceed. To make a new configuration instead, cancel and then change the configuration name.';
           } // this works
           if (confirm(confirmMsg)) {
-             inputTextToSave = document.getElementById('inputTextToSave').value;
+             this.inputTextToSave = document.getElementById('inputTextToSave').value;
+             axios.post(`http://localhost:8000/configs/${this.configName}`, this.inputTextToSave.value).then((response) => {
+              responseStatus = response.status; // this works
+              });
             document.getElementById('inputTextToSave').value = 'File saved.';
+            document.getElementById('configNameBox').value = '';
           }
           // not sure how to send the text data back to the server
         }
@@ -54,13 +65,13 @@
       validateConfig() {
         const helper = new DOMParser();
         try {
-          XMLconfig = helper.parseFromString(inputTextToSave, 'text/xml'); // this never throws an exception
+          this.XMLconfig = helper.parseFromString(this.inputTextToSave, 'text/xml'); // this never throws an exception
           // alert(inputTextToSave.toString());
-          didValidate = 1;
+          this.didValidate = 1;
           alert('Validation Complete');
         } catch (e) {
           alert('XML Parsing Error');
-          didValidate = 0;
+          this.didValidate = 0;
           // alert(e.getError());
           // alert(result.value.toString());
         }

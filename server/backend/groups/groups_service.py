b735dd6cc9bd3011082f9f437c7dde21d3694ad2
Chandler Butler <chandler.butler@ruralsource.com>
2019-04-10 16:01:17 -0400

    oops logic was backwards

--- server/backend/groups/groups_service.py
+++ server/backend/groups/groups_service.py
@@ -4,18 +4,19 @@ from heartbeat.models import Group, Sysmon, Configuration
 def create_group(name, json):
     try:
         group = Group.objects.get(NAME=name)
+        return -1
+        
+    except Exception as e:
         try:
-            sysmon = Sysmon.objects.get(VERSION = json['sysmon_version']).VERSION
+            sysmon = Sysmon.objects.get(VERSION = json['sysmon_version'])
             try:
-                config = Configuration.objects.get(NAME = json['configuration']).NAME
+                config = Configuration.objects.get(NAME = json['configuration'])
                 new_group = Group(NAME = name,
                                   CONFIGURATION = config,
                                   SYSMON = sysmon)
+                new_group.save()
                 return 1
             except Exception as e:
                 return -1
         except Exception as e:
             return -1
-        
-    except Exception as e:
-        return -1

e492ab20a066947b2a2e3037a0732fb728137baa
William Wernert <william.wernert@gmail.com>
2019-04-18 19:36:53 -0400

    William/bugfix (#61)
    * Fixed several backend issues and made sure the server can run from startup without manipulating the db

--- server/backend/groups/urls.py
+++ server/backend/groups/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('<str:name>', views.creation, name='create_group'),
     path('', views.index, name="index"),
-    path('<str:agent>/<str:group>',views.associate, name='associate'),
+    path('<str:agent>/<str:group>', views.associate, name='associate'),
 ]

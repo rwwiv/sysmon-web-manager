5290afa6f5083b67f7e2cc026c9906fe098ffddc
Aubrey Bryant <aubbryant@augusta.edu>
2019-04-19 23:49:56 -0400

    making the file

---
+++ server/frontend/src/components/VersionList.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="box">
+    <div class="box-header with-border">
+      <h4 class="box-title">
+        Sysmon Versions
+      </h4>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+      <div class="col-auto">
+        <router-link to="ConfigEditor" active-class="active" button class="btn btn-secondary pull-right">Check for updates to Sysmon.</router-link>
+      </div>
+      <table class="table no-margin">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th></th>
+            <th class = "center-text icon-column">View/Edit</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="config in configs" :key="config.name">
+            <td>{{ config.name }}</td>
+            <td v-if="config.default">
+              <span class="label label-primary" v-if="config.default">
+                Default
+              </span>
+            </td>
+            <td v-else></td>
+            <td class="center-text">
+              <router-link :to="{name: 'ConfigEditor', params: {id: config.name}}"
+                           active-class="active">
+                <a>
+                  <i class="fa fa-search-plus"></i>
+                </a>
+              </router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<script>
+  import axios from 'axios';
+
+export default {
+  name: 'ConfigsList',
+  data() {
+    return {
+      configs: [],
+      errors: [],
+      editConfig: '',
+      viewConfig: '',
+    };
+  },
+  methods: {
+    getAllConfigs() {
+      axios.get('http://localhost:8000/configs')
+        .then((response) => {
+          this.configs = response.data;
+          console.log(response.data);
+        })
+        .catch((e) => {
+          this.errors.push(e);
+        });
+    },
+  },
+  mounted() {
+  this.getAllConfigs();
+  },
+};
+</script>
+
+<style scoped>
+  td a {
+    cursor:pointer;
+  }
+  .center-text {
+    text-align:center;
+  }
+  .icon-column{
+    width:10%;
+  }
+</style>
+
+<style scoped>
+  td a {
+    cursor:pointer;
+  }
+  .center-text {
+    text-align:center;
+  }
+  .icon-column{
+    width:10%;
+  }
+</style>
